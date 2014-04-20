05c59a530675b3f5ee44117abfa6a322485e1784
Jonny Lu <johnnyluu@users.noreply.github.com>
2014-04-20 19:02:01 +1000

    UI update
    Android style title bar and side menu; slide-down info; progress bar

--- js/tuition.js
+++ js/tuition.js
@@ -4,3 +4,33 @@ var SILLY = (function(module){
 	};
 	return module;
 }(SILLY || {}));
+
+$('#silly').click(function(){
+	SILLY.DoIt($('#results'));
+	if ($('#clear').css("color") == 'rgb(169, 169, 169)'){
+		$('#clear').css("color","black");
+		$('#clear').css("background",'lightgrey');
+	};
+});
+
+$('#clear').click(function(){
+	$('#results').html('');
+	$('#clear').css("background","grey");
+	$('#clear').css("color","darkgrey");
+});
+
+$('#title').click(function(){
+	if ($('header').hasClass('active')){
+		$('header').removeClass('active');
+	} else { $('header').addClass('active');}
+});
+
+$('#logo').click(function(){
+	if ($(this).hasClass('active')){
+		$(this).removeClass('active');
+		$('#slidingmenu').removeClass('active');
+	} else {
+		$(this).addClass('active');
+		$('#slidingmenu').addClass('active');
+	}
+});
